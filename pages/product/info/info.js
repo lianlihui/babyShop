32030d25e2a1bf28ec415ebffe760176218e70d0
psx <pansenxin.com>
2017-11-02 22:54:23 +0800

    commit files model

--- pages/product/info/info.js
+++ pages/product/info/info.js
@@ -1,3 +1,30 @@
+var app = getApp();
+
 Page({
 
-});
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    modalSpecShow:false
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onLoad: function (options) {
+
+  },
+
+  openModal: function () {
+    this.setData({
+      modalSpecShow: true
+    });
+  },
+
+  closeModal: function (e, modalName) {
+    this.setData({
+      modalSpecShow: false
+    });
+  },
+})
