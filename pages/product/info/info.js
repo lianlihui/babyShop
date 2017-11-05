1c00389127a9c1f6b8a95a9648a4602eb79e3e60
lifylian <377834110@qq.com>
2017-11-05 17:19:37 +0800

    commit files

--- pages/product/info/info.js
+++ pages/product/info/info.js
@@ -6,14 +6,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    modalSpecShow:false
+    modalSpecShow:false,
+    id: 0, //产品id
+    warebean: null //产品详情
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onLoad: function (options) {
+    this.setData({
+      id: options.id
+    });
+  },
 
+  onShow: function() {
+    this.getInfoData();
+  },
+
+  //获取产品详情信息
+  getInfoData: function() {
+    var self = this;
+    var postData = {
+      wareid: self.data.id
+    };
+    
+    //获取首页数据    
+    app.ajax({
+      url: app.globalData.serviceUrl + 'mWareDetail.html',
+      data: postData,
+      method: 'GET',
+      successCallback: function(res) {
+        self.setData({
+          // imgUrls: res.data.poslinklist,
+          imageRootPath: res.imageRootPath,
+          warebean: res.warebean
+        });
+      },
+      failCallback: function(res) {
+        console.log(res);
+      }
+    });
   },
 
   openModal: function () {
--- pages/product/list/list.js
+++ pages/product/list/list.js
@@ -1,3 +1,39 @@
-Page({
+//index.js
+//获取应用实例
+var app = getApp();
 
-});
+Page({
+  data: {
+    'imageRootPath': '',
+    'warelist': []
+  },
+  onShow: function() {
+    // wx.showLoading({title: '页面加载中', mask: true})
+    this.getIndexData();
+  },
+
+  getIndexData: function() {
+    var self = this;
+    var postData = {
+      waretypeid: ''
+    };
+    
+    //获取首页数据    
+    app.ajax({
+      url: app.globalData.serviceUrl + '/mwarelist.html',
+      data: postData,
+      method: 'GET',
+      successCallback: function(res) {
+        self.setData({
+          imageRootPath: res.data.imageRootPath,
+          warelist: res.data.warelablelist
+        });
+      },
+      failCallback: function(res) {
+        console.log(res);
+      }
+    });
+
+  }
+
+})
